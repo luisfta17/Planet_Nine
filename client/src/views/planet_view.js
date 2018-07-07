dec461bc3e11f020b0794ea092a5f713f09c15c7
KatyPreston <prestonkj@hotmail.com>
2018-07-07 13:40:51 +0100

    dropdown planet id's working

--- client/src/views/planet_view.js
+++ client/src/views/planet_view.js
@@ -11,13 +11,22 @@ PlanetView.prototype.bindEvents = function () {
     this.planets = evt.detail;
      this.renderInitialInfo(this.planets);
   });
+
+    const dropdown = document.querySelectorAll('.dropdown');
+    for (planet of dropdown) {
+      planet.addEventListener('click', (evt) =>{
+        console.log(evt.target.id);
+        const planetInfoView = new PlanetInfoView(this.container);
+        planetInfoView.initialRender(this.planets, evt.target.id);
+      })
+    }
 };
 
 
 PlanetView.prototype.renderInitialInfo = function (planets) {
   this.container.innerHTML = '';
   const planetInfoView = new PlanetInfoView(this.container);
-  planetInfoView.initialRender(planets, 0)
+  planetInfoView.initialRender(this.planets, 0)
 };
 
 module.exports = PlanetView;
