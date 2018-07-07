e726f677a22e77fe5bc1d0ff5dda01dece6e90be
Luis Tejero <luisfaridta@gmail.com>
2018-07-07 13:55:18 +0100

    added fetch to server side to be able to request info

---
+++ client/src/models/hubble.js
@@ -0,0 +1,20 @@
+const PubSub = require('../helpers/pub_sub');
+const Request = require('../helpers/request');
+
+
+const Hubble = function(url){
+  this.url = url;
+};
+
+Hubble.prototype.getData = function () {
+  const request = new Request(this.url);
+  request.get().then((picture) => {
+    console.log(picture);
+    PubSub.publish('Hubble:data-loaded', picture);
+  })
+  .catch(console.error);
+};
+
+
+
+module.exports = Hubble;
