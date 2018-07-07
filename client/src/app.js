94e78f41832aa9fb959fb0e4f86311aee246700b
KatyPreston <prestonkj@hotmail.com>
2018-07-07 11:30:57 +0100

    dropdown planets added

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,14 @@
 const Planets = require('./models/planets');
-const APOD = require('./models/apod');
-const Hubble = require('./models/hubble');
+// const APOD = require('./models/apod');
+// const Hubble = require('./models/hubble');
 const PlanetView = require('./views/planet_view');
-const ApodView = require('./views/apod_view');
-const HubbleView = require('./views/hubble_view');
+// const ApodView = require('./views/apod_view');
+// const HubbleView = require('./views/hubble_view');
 
 document.addEventListener('DOMContentLoaded', () => {
 const displayContainer = document.querySelector('#display-container');
-const apodSelector = document.querySelector('#apod');
-const hubbleSelector = document.querySelector('#hubble');
+// const apodSelector = document.querySelector('#apod');
+// const hubbleSelector = document.querySelector('#hubble');
 
 const planets = new Planets('http://localhost:3000/api/planets');
 planets.getData();
@@ -16,21 +16,21 @@ planets.getData();
 const planetView = new PlanetView(displayContainer);
 planetView.bindEvents();
 
-apodSelector.addEventListener('click', () => {
-  const apod = new APOD('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
-  apod.getData();
-
-  const apodView = new ApodView(displayContainer);
-  apodView.bindEvents();
-});
-
-hubbleSelector.addEventListener('click', () => {
-  const hubble = new Hubble('http://hubblesite.org/api/v3/image/');
-  hubble.getData();
-
-  const hubbleView = new HubbleView(displayContainer);
-  hubbleView.bindEvents();
-});
+// apodSelector.addEventListener('click', () => {
+//   const apod = new APOD('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+//   apod.getData();
+//
+//   const apodView = new ApodView(displayContainer);
+//   apodView.bindEvents();
+// });
+//
+// hubbleSelector.addEventListener('click', () => {
+//   const hubble = new Hubble('http://hubblesite.org/api/v3/image/');
+//   hubble.getData();
+//
+//   const hubbleView = new HubbleView(displayContainer);
+//   hubbleView.bindEvents();
+// });
 
 
 });
--- client/src/views/planet_view.js
+++ client/src/views/planet_view.js
@@ -1,16 +1,18 @@
 const PubSub = require('../helpers/pub_sub');
 const PlanetInfoView = require('./planet_info_view');
 
-const PlanetView = function(container){
+const PlanetView = function(container, planets){
   this.container = container;
+  this.planets = planets;
 };
 
 PlanetView.prototype.bindEvents = function () {
   PubSub.subscribe('Planets:data-loaded', (evt) => {
-     this.renderInitialInfo(evt.detail);
+    this.planets = evt.detail;
+     this.renderInitialInfo(this.planets);
   });
-
 };
+
 
 PlanetView.prototype.renderInitialInfo = function (planets) {
   this.container.innerHTML = '';
