2ce32a7d2ee8dfeba74c6d1e2f0250ded1b2d929
Luis Tejero <luisfaridta@gmail.com>
2018-07-09 14:45:45 +0100

    video added

--- client/src/views/iss_info_view.js
+++ client/src/views/iss_info_view.js
@@ -10,6 +10,7 @@ IssInfoView.prototype.initialRender = function (iss) {
   this.createTemplate();
   this.createDiv("mapid");
   this.createMap(iss);
+  this.addVideo();
   const returnButton = this.createButton("Return to Main Page");
   returnButton.addEventListener('click', () => {
     window.location.href = "/";
@@ -24,7 +25,6 @@ IssInfoView.prototype.createMap = function (iss) {
   let latitude = iss.iss_position.latitude
   let longitude = iss.iss_position.longitude
   var mymap = L.map('mapid').setView([latitude, longitude], 2);
-
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -91,6 +91,20 @@ IssInfoView.prototype.createDiv = function (id) {
   this.container.appendChild(div);
 };
 
+IssInfoView.prototype.addVideo = function () {
+  const iframe= document.createElement('iframe');
+  iframe.width = "1080";
+  iframe.height = "600";
+  iframe.src = "https://www.ustream.tv/embed/17074538?html5ui";
+  iframe.scrolling = "no";
+  iframe.style = "border: 0 none transparent;";
+  iframe.frameborder = "0";
+  iframe.allowfullscreen = true;
+  iframe.webkitallowfullscreen = true;
+  this.container.appendChild(iframe);
+};
+// <iframe width="480" height="270" src="https://www.ustream.tv/embed/17074538?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
+
 IssInfoView.prototype.createButton = function (text) {
   const button = document.createElement('button');
   button.textContent = text;
@@ -100,7 +114,8 @@ IssInfoView.prototype.createButton = function (text) {
 
 IssInfoView.prototype.createTemplate = function () {
   this.createTitle("ISS TRACKER");
-  // this.createParagraph("The Hubble Space Telescope is a large telescope in space. It was launched into orbit by space shuttle Discovery on April 24, 1990. Hubble orbits about 547 kilometers (340 miles) above Earth. It is the length of a large school bus and weighs as much as two adult elephants. Hubble travels about 5 miles per second: That is like traveling from the eastern coast of the United States to the western coast in 10 minutes. Hubble is solar-powered. Hubble takes sharp pictures of objects in the sky such as planets, stars and galaxies. Hubble has made more than one million observations. These include detailed pictures of the birth and death of stars, galaxies billions of light years away, and comet pieces crashing into Jupiter's atmosphere. Scientists have learned a lot about the universe from these pictures. Many of them are beautiful to look at.")
+  this.createParagraph("The International Space Station (ISS) is a multi-nation construction project that is the largest single structure humans ever put into space. Its main construction was completed between 1998 and 2011, although the station continually evolves to include new missions and experiments. It has been continuously occupied since Nov. 2, 2000.As of January 2018, 230 individuals from 18 countries have visited the International Space Station. Top participating countries include the United States (145 people) and Russia (46 people). Astronaut time and research time on the space station is allocated to space agencies according to how much money or resources (such as modules or robotics) that they contribute. The ISS includes contributions from 15 nations. NASA (United States), Roscosmos (Russia) and the European Space Agency are the major partners of the space station who contribute most of the funding; the other partners are the Japanese Aerospace Exploration Agency and the Canadian Space Agency.Current plans call for the space station to be operated through at least 2024, with the partners discussing a possible extension until 2028. Afterwards, plans for the space station are not clearly laid out. It could be deorbited, or recycled for future space stations in orbit.");
+  
   // this.createSubtitle("What Are Hubble's Most Important Discoveries?");
   // this.createParagraph("Images taken by Hubble have helped scientists estimate the age and size of the universe. Scientists believe the universe is almost 14 billion years old. Hubble has helped scientists understand how planets and galaxies form. An image called 'Hubble Ultra Deep Field' shows the farthest galaxies ever seen. Hubble has detected black holes, which suck in everything around them, including light. The telescope has played a key role in the discovery of dark energy, a mysterious force that causes the universe to expand faster and faster as time goes on. And it has revealed details of gamma-ray bursts -- powerful explosions of energy that occur when massive stars collapse. Hubble has also studied the atmospheres of planets revolving around stars similar to Earth’s sun.")
   // this.createSubtitle("Random picture taken from Hubble Space Telescope, powered by NASA - HubbleSite.org ")
